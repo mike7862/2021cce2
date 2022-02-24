cfe6fc1fc95a4375483d2959629fe3f56cbad053
mike7862 <91170810+mike7862@users.noreply.github.com>
2022-02-24 17:28:30 +0800

    Add files via upload

---
+++ week01/week01-1.cpp
@@ -0,0 +1,10 @@
+///week01-1 step01-1
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("使用著輸入一個數值: ");
+    scanf("%d",&a);
+    printf("你讀到了 %d\n",a);
+    return 0;
+}
---
+++ week01/week01-2.cpp
@@ -0,0 +1,15 @@
+///week01-2 step01-2
+#include <stdio.h>
+int main()
+{
+ ///   int a;
+ ///   printf("使用著輸入一個數值: ");
+ ///   scanf("%d",&a);
+ ///  printf("你讀到了 %d\n",a);
+ ///   return 0;
+
+ char c;
+ printf("請輸入一堆字: ");
+ scanf("%c",&c);
+ printf("你讀到了 %c\n",c);
+}
