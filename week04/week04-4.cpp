81306514615f2248ac0ec0d9b6a95467e63ef3b4
mike7862 <91170810+mike7862@users.noreply.github.com>
2022-03-17 17:35:23 +0800

    Add files via upload

---
+++ week04/week04-4.cpp
@@ -0,0 +1,14 @@
+///week04-4 step03-1
+#include <stdio.h>
+struct DATA{
+    char c;
+    int ans;
+};
+
+struct DATA list1;
+struct DATA lists[100];
+int main()
+{
+    list1.c='A';
+    list1.ans=1;
+}
