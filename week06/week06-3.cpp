b645ca04b4697411cbf61401be6465e62c6dc85c
mike7862 <91170810+mike7862@users.noreply.github.com>
2022-03-31 17:26:39 +0800

    Add files via upload

---
+++ week06/week06-3.cpp
@@ -0,0 +1,25 @@
+///week06-3.cpp step02-1
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char words[1000][50];
+int compare( const void *p1, const void *p2 )
+{
+	char * s1 = (char*) p1;
+	char * s2 = (char*) p2;
+	return strcmp(s1,s2);
+}
+int main()
+{
+	int N;
+	scanf("%d",&N);
+	for(int i=0;i<N;i++){
+		scanf("%s",words[i] );
+	}
+		qsort( words, N, 50, compare);
+
+	for(int i=0;i<N;i++){
+		printf("%s\n",words[i] );
+	}
+	return 0;
+}
