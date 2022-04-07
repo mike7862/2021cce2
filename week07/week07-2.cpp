3d7126c14353bfcc7eb9d5e8d7c110fbfc3bb89a
mike7862 <mikelai1012@gmail.com>
2022-04-07 17:38:57 +0800

    第七週

---
+++ week07/week07-2.cpp
@@ -0,0 +1,25 @@
+///week07-2.cpp step02-2
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char line[2000][80];
+int compare( const void *p1,const void *p2)
+{
+	return strcmp( (char*)p1,(char*)p1);
+}
+int main()
+{
+	int N;
+	scanf("%d\n",&N);
+
+	for(int i=0;i<N;i++){
+		gets( line[i] );
+	}
+
+	qsort( line, N, 80, compare);
+
+	for(int i=0;i<N;i++){
+		printf("%s\n",line[i]);
+	}
+	return 0;
+}
---
+++ week07/week07-3.cpp
@@ -0,0 +1,27 @@
+///week07-3.cpp step02-3
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char line[2000][80];
+int compare( const void *p1,const void *p2)
+{
+	return strcmp( (char*)p1,(char*)p1);
+}
+int main()
+{
+	int N;
+	scanf("%d\n",&N);
+
+	for(int i=0;i<N;i++){
+		scanf("%s",line[i]);
+		char others[80];
+		gets( others );
+	}
+
+	qsort( line, N, 80, compare);
+
+	for(int i=0;i<N;i++){
+		printf("%s\n",line[i]);
+	}
+	return 0;
+}
