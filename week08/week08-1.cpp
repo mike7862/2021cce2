1698311799de8c12974d757da7dd2143d55a5484
mike7862 <91170810+mike7862@users.noreply.github.com>
2022-04-14 17:29:32 +0800

    Add files via upload

---
+++ week08/week08-1.cpp
@@ -0,0 +1,14 @@
+///week08-1.cpp step02-1
+#include <stdio.h>
+int main()
+{
+    int grade;
+    for(int i=0;i<3;i++){
+        scanf("%d",&grade);
+    }
+
+    for(int i=0;i<3;i++){
+        printf("%d\n",grade);
+    }
+}
+
---
+++ week08/week08-4.cpp
@@ -0,0 +1,9 @@
+///week08-4.cpp
+#include <stdio.h>
+int main()
+{
+    FILE * fout=fopen("檔名.txt","w+");
+    fprintf(fout, "Hello 我在檔案裡,哈哈\n");
+
+    printf("Hello World");
+}
